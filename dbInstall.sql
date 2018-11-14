7d5b00d0024a5f47826c56b132cfc0baba8675e5
Michał Hordyjewicz <hordyjewiczmichal@gmail.com>
2018-11-14 22:46:15 +0100

    dbinstall fix

--- dbInstall.sql
+++ dbInstall.sql
@@ -1,17 +1,16 @@
--- DROP TABLE IF EXISTS `polish`;
--- polish: table
+ALTER DATABASE `${data_base_name}` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
 CREATE TABLE `polish` (
-  `id`            bigint(20)                                    NOT NULL AUTO_INCREMENT,
-  `word`          varchar(30) COLLATE utf8mb4_unicode_ci        NOT NULL,
-  `add_date`      datetime                                      NOT NULL,
-  `adder_ip`      int(11) unsigned                              NOT NULL DEFAULT '0',
-  `accepted`      enum ('yes', 'no') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'no',
-  `reject_reason` varchar(255) COLLATE utf8mb4_unicode_ci                DEFAULT NULL,
-  `review_date`   datetime                                               DEFAULT NULL,
+  `id`            bigint(20)                                       NOT NULL AUTO_INCREMENT,
+  `word`          varchar(30) COLLATE utf8mb4_bin                  NOT NULL,
+  `add_date`      datetime                                         NOT NULL,
+  `adder_ip`      int(11) unsigned                                 NOT NULL DEFAULT '0',
+  `accepted`      enum ('yes', 'no') COLLATE utf8mb4_bin           NOT NULL DEFAULT 'no',
+  `reject_reason` varchar(255) COLLATE utf8mb4_bin                          DEFAULT NULL,
+  `review_date`   datetime                                                  DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `word` (`word`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_pl_0900_as_cs;
-
+  COLLATE = utf8mb4_bin;
