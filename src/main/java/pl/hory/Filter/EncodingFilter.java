1b831a82e96c7f4e5ad188acd234f0ef95407552
Michał Hordyjewicz <hordyjewiczmichal@gmail.com>
2018-11-15 18:50:00 +0100

    pom and java encoding fix

--- src/main/java/pl/hory/Filter/EncodingFilter.java
+++ src/main/java/pl/hory/Filter/EncodingFilter.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.time.LocalDateTime;
+import java.util.Enumeration;
+import java.util.Properties;
 
 @WebFilter(filterName = "EncodingFilter", urlPatterns = {"/*"})
 public class EncodingFilter implements Filter
@@ -19,6 +21,20 @@ public class EncodingFilter implements Filter
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException
     {
+        Properties properties = java.lang.System.getProperties();
+
+        System.out.println(properties.getProperty("file.encoding"));
+        properties.setProperty("file.encoding", "UTF-8");
+        System.out.println(properties.getProperty("file.encoding"));
+
+//        Enumeration<?> enumeration = properties.propertyNames();
+//        while (enumeration.hasMoreElements())
+//        {
+//            String key = (String) enumeration.nextElement();
+//            System.out.println(key + " : " + properties.getProperty(key));
+//        }
+        // JAVA PROPERTIES
+
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=UTF-8");
         resp.setCharacterEncoding("UTF-8");
